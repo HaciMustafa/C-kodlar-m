d2f2d8a42f67a2a0aff5433ced92a0e1a828382e
HacıMustafa <hazel.1589.360@gmail.com>
2020-09-12 20:06:20 +0300

    Update Hello World.cpp

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<iostream >
 using namespace std;
 
@@ -6,3 +7,13 @@ int main(){
 	cout<<"Hello World!!!";
 	
 }
+=======
+#include<iostream >
+using namespace std;
+
+int main(){
+	
+	cout<<"Hello World!!!";
+	
+}
+>>>>>>> parent of e8330ef... v1.0
